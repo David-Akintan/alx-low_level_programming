798e629ee9aaa2bb0622491478ce16f39300651e
David <akintandavid96@gmail.com>
2022-04-07 18:22:50 -0700

    Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+/*
+ * Description:
+ * This program prints the size of the data types present in C
+ * */
 
-int main() {
+int main(void)
+{
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long double: %d byte(s)\n", sizeof(long double));
-	printf("Size of a unsigned short int: %d byte(s)\n", sizeof(unsigned short int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	printf("size of a long long int : %d byte(s)\n", sizeof(long long int));
-	printf("Size of a double: %d byte(s)\n", sizeof(double));
-	printf("size of a short int : %d byte(s)\n", sizeof(short int));  
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	return 0;
 }
-
